1e21764fdc51ff048dcfbfb9c762d2152c07ff16
Tonia Joseph <tonia.joseph@reflectionsinfos.com>
2025-08-11 15:25:08 +0530

    resolved build errors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 import { Request, Response } from "express";
 import { serve } from "inngest/express";
 import { inngest } from "./inngest/index";
