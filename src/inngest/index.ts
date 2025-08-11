10afdc6921a5b595c91c15d145f283ec005d594d
Tonia Joseph <tonia.joseph@reflectionsinfos.com>
2025-08-11 19:22:52 +0530

    bug fix

--- src/inngest/index.ts
+++ src/inngest/index.ts
@@ -1,7 +1,11 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "ai-therapy-agent" });
+export const inngest = new Inngest({
+  id: "ai-therapy-agent",
+  eventKey:
+    "dA5UuoXog5-5cCs1p5NRJN3A_1jsnDIBkS2uIUskh3mtVWfqdkTNwq1mnhd6p9mHv-wQ4tju_6T3EdYF4B3Wsg",
+});
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [];
