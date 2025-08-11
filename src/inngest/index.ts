4ffe443610dbb67818eee2f9f27ebf3e8e14572f
Tonia Joseph <tonia.joseph@reflectionsinfos.com>
2025-08-11 20:21:52 +0530

    add eventkey

--- src/inngest/index.ts
+++ src/inngest/index.ts
@@ -3,8 +3,7 @@ import { Inngest } from "inngest";
 // Create a client to send and receive events
 export const inngest = new Inngest({
   id: "ai-therapy-agent",
-  eventKey:
-    "dA5UuoXog5-5cCs1p5NRJN3A_1jsnDIBkS2uIUskh3mtVWfqdkTNwq1mnhd6p9mHv-wQ4tju_6T3EdYF4B3Wsg",
+  eventKey: process.env.INNGEST_EVENT_KEY,
 });
 
 // Create an empty array where we'll export future Inngest functions
